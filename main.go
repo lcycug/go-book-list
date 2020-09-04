40f64eadff0f8d58f280554852619029e37368d2
Chenyang Li <lcycug@live.com>
2020-09-04 21:50:05 +0800

    Work with DB

--- main.go
+++ main.go
@@ -21,7 +21,6 @@ type Book struct {
 	Year   int       `json:"year"`
 }
 
-var books []Book
 var db *sql.DB
 
 func init() {
@@ -60,21 +59,20 @@ func main() {
 func removeBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
+	var uId uuid.UUID
 	params := mux.Vars(r)
 	if sId, ok := params["id"]; ok {
-		uId, err := uuid.Parse(sId)
+		var err error
+		uId, err = uuid.Parse(sId)
 		if err != nil {
 			log.Println(err)
-			_ = json.NewEncoder(w).Encode(&books)
+			_, _ = fmt.Fprintf(w, "Bad ID")
 			return
 		}
-		for index, book := range books {
-			if book.ID == uId {
-				books = append(books[:index], books[index+1:]...)
-			}
-		}
+		_, err = db.Exec("delete from books where id=$1", uId)
+		logFatal(err)
 	}
-	_ = json.NewEncoder(w).Encode(&books)
+	_, _ = fmt.Fprintf(w, "Record with ID %q has beed removed from the database.", uId.String())
 }
 
 // Update book(s)
@@ -83,15 +81,11 @@ func updateBooks(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	var newBooks []*Book
 	_ = json.NewDecoder(r.Body).Decode(&newBooks)
-	for ni, n := range newBooks {
-		for oi, o := range books {
-			if n.ID == o.ID {
-				books[oi] = *newBooks[ni]
-				break
-			}
-		}
+	for _, n := range newBooks {
+		_, err := db.Exec("update books set title=$1, author=$2, year=$3 where id=$4", &n.Title, &n.Author, &n.Year, &n.ID)
+		logFatal(err)
 	}
-	_ = json.NewEncoder(w).Encode(&books)
+	_ = json.NewEncoder(w).Encode(&newBooks)
 }
 
 // Add book(s)
@@ -102,15 +96,17 @@ func addBooks(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&newBooks)
 	for _, book := range newBooks {
 		book.ID = uuid.New()
-		books = append(books, *book)
+		db.QueryRow("insert into books (id, title, author, year) values ($1, $2, $3, $4)", book.ID, book.Title, book.Author, book.Year)
 	}
-	_ = json.NewEncoder(w).Encode(&books)
+	_ = json.NewEncoder(w).Encode(&newBooks)
 }
 
 // Get a single book
 func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
+	var matchedBook []Book
+	var book Book
 	param := mux.Vars(r)
 	if id, ok := param["id"]; ok {
 		uId, err := uuid.Parse(id)
@@ -118,23 +114,25 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 			_, _ = fmt.Fprintf(w, "Invalid ID")
 			return
 		}
-		for _, book := range books {
-			if book.ID == uId {
-				_ = json.NewEncoder(w).Encode(&book)
-				return
-			}
+		rows, err := db.Query("select * from books where id=$1", uId)
+		logFatal(err)
+		for rows.Next() {
+			err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+			logFatal(err)
+			matchedBook = append(matchedBook, book)
 		}
+		_ = json.NewEncoder(w).Encode(&matchedBook)
+		return
 	}
 	_, _ = fmt.Fprintf(w, "No book found by the ID")
 }
 
 // Get all books
-func getBooks(w http.ResponseWriter, r *http.Request) {
+func getBooks(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	var book Book
 	var books []Book
-
 	rows, err := db.Query("select * from books")
 	defer rows.Close()
 	logFatal(err)
